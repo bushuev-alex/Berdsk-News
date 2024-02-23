52caba3a7ec0199f10b5e0b33f3d75100e6c9ce4
bushuev-alex <bushuev-alex@mail.ru>
2024-02-23 11:10:48 +0700

    1. register new models to admin.py

--- berdsk_news/news/admin.py
+++ berdsk_news/news/admin.py
@@ -4,12 +4,13 @@ from .models import (Author,
                      Category,
                      Photo,
                      News,
+                     Origin,
+                     Tag,
                      NewsCategory,
-                     NewsPhotos,
-                     Comment,
-                     CmntToCmnt,
+                     NewsTag,
                      Advertiser,
                      Advertisement,
+                     Search
                      )
 
 
@@ -18,9 +19,10 @@ admin.site.register(Author)
 admin.site.register(Category)
 admin.site.register(Photo)
 admin.site.register(News)
+admin.site.register(Origin)
+admin.site.register(Tag)
+admin.site.register(Search)
 admin.site.register(NewsCategory)
-admin.site.register(NewsPhotos)
-admin.site.register(Comment)
-admin.site.register(CmntToCmnt)
+admin.site.register(NewsTag)
 admin.site.register(Advertiser)
 admin.site.register(Advertisement)
