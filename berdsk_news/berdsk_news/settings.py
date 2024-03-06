3c1a52609335507b480c17bfe4522704aa49137c
bushuev-alex <bushuev-alex@mail.ru>
2024-03-06 23:17:31 +0700

    1. update allowed hosts

--- berdsk_news/berdsk_news/settings.py
+++ berdsk_news/berdsk_news/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = DJ_SCRT_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', "192.168.0.40", "212.20.45.230"]
 
 # Application definition
 
