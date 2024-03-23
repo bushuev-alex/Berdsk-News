52a2d3402921891c99585473bb072b6b65325c61
bushuev-alex <bushuev-alex@mail.ru>
2024-03-23 11:04:37 +0700

    1. update parser settings: set user_agent = some_data download_delay=3

--- berdsk_news/parser/parser/settings.py
+++ berdsk_news/parser/parser/settings.py
@@ -26,7 +26,8 @@ NEWSPIDER_MODULE = "parser.spiders"
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = "parser (+http://www.yourdomain.com)"
+USER_AGENT = ("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) "
+              "Chrome/120.0.0.0 YaBrowser/24.1.0.0 Safari/537.36")
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
@@ -37,7 +38,7 @@ ROBOTSTXT_OBEY = False
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 2
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
