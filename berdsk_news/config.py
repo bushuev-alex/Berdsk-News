32c61d9a12f19c1fc6040c677b3f11346569cce8
bushuev-alex <bushuev-alex@mail.ru>
2024-04-07 23:32:31 +0700

    1. add EMAIL_PSWD to config.py

--- berdsk_news/config.py
+++ berdsk_news/config.py
@@ -8,6 +8,7 @@ DB_PASS = os.getenv('DB_PASS')
 DB_HOST = os.getenv('DB_HOST')
 DB_PORT = os.getenv('DB_PORT')
 DB_NAME = os.getenv('DB_NAME')
+EMAIL_PSWD = os.getenv('EMAIL_PSWD')
 
 DJ_SCRT_KEY = os.getenv('DJ_SCRT_KEY')
 PARSER_CWD = os.getenv('PARSER_CWD')
