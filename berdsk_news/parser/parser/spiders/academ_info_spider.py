4eba9d2f5fc0806cf12727fbf5c4d567e1501b6b
bushuev-alex <bushuev-alex@mail.ru>
2024-09-21 00:43:44 +0700

    1. add headers and separate cookies info to academinfo spider

--- berdsk_news/parser/parser/spiders/academ_info_spider.py
+++ berdsk_news/parser/parser/spiders/academ_info_spider.py
@@ -11,23 +11,68 @@ from datetime import datetime  # , timedelta
 
 class AcademInfoSpider(scrapy.Spider):
     name: str = "academinfo"
-    base_url = "https://academ.info"
-    headers: dict = {"Referer": "https://academ.info/news/",
+    base_url: str = "https://academ.info"
+    headers: dict = {"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,"
+                               "image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+                     "Accept-Encoding": "gzip, deflate, br, zstd",
                      'User-Agent': "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) "
-                                   "Chrome/116.0.5845.1028 YaBrowser/23.9.1.1028 (beta) Yowser/2.5 Safari/537.36"}
+                                   "Chrome/120.0.0.0 YaBrowser/24.1.0.0 Safari/537.36",
+                     "Referer": "https://academ.info/news/?PAGEN_1=2",
+                     "Cookie": "_ym_debug=null; scroll_block=null; _ym_uid=1693631394390615044; "
+                               "adtech_uid=61336ad4-ba51-47c2-9ce4-b5313907fd47%3Aacadem.info; _ym_d=1711187398; "
+                               "top100_id=t1.7685730.747803947.1722695285209; beget=begetok; _ga=GA1.1.863834965.1724166611;"
+                               " "
+                               "BX_USER_ID=7eb99dbbc4a602ad0f54e2fa4611ce38; tmr_lvid=075530c4775067db18a536a963f39d9f; "
+                               "tmr_lvidTS=1693631394013; domain_sid=PffTnYB340EEP_OIPUv5-%3A1726381092336; "
+                               "_ac_oid=cbcc7eec63a09fa33505e87aa5e5eba0%3A1726388470964; "
+                               "_ga_X5NR8KKRSN=GS1.1.1726498933.7.0.1726498933.0.0.0; _ym_debug=null; scroll_block=null; "
+                               "PHPSESSID=7070dd5505168af1750ed778715b3455; BITRIX_CONVERSION_CONTEXT_s1=%7B%22ID%22%3A5%2"
+                               "C%22EXPIRE%22%3A1726765140%2C%22UNIQUE%22%3A%5B%22conversion_visit_day%22%5D%7D; "
+                               "t3_sid_7685730=s1.834295303.1726759296749.1726760789872.17.48",
+                     }
+    cookies: dict = {"_ym_debug": "null",
+                     "scroll_block": "null",
+                     "_ym_uid": "1693631394390615044",
+                     "adtech_uid": "61336ad4-ba51-47c2-9ce4-b5313907fd47%3Aacadem.info",
+                     "_ym_d": "1711187398",
+                     "top100_id": "t1.7685730.747803947.1722695285209",
+                     "beget": "begetok",
+                     "_ga": "GA1.1.863834965.1724166611",
+                     "BX_USER_ID": "7eb99dbbc4a602ad0f54e2fa4611ce38",
+                     "tmr_lvid": "075530c4775067db18a536a963f39d9f",
+                     "tmr_lvidTS": "1693631394013",
+                     "domain_sid": "PffTnYB340EEP_OIPUv5-%3A1726381092336",
+                     "_ac_oid": "cbcc7eec63a09fa33505e87aa5e5eba0%3A1726388470964",
+                     "_ga_X5NR8KKRSN": "GS1.1.1726498933.7.0.1726498933.0.0.0",
+                     "PHPSESSID": "7070dd5505168af1750ed778715b3455",
+                     "BITRIX_CONVERSION_CONTEXT_s1": "%7B%22ID%22%3A5%2C%22EXPIRE%22%3A1726765140%2C%22UNIQUE%22%3A%5B%22conversion_visit_day%22%5D%7D",
+                     "t3_sid_7685730": "s1.834295303.1726759296749.1726760789872.17.48",
+                     }
 
     start_urls = ["https://academ.info/news/",
-                  # "https://academ.info/news/?PAGEN_1=2",
+                  "https://academ.info/news/?PAGEN_1=2",
                   ]
 
-    async def parse(self, response, **kwargs):
-        news_list = response.css("div.grid-list__item")
+    def start_requests(self):
+        if not self.start_urls and hasattr(self, "start_url"):
+            raise AttributeError(
+                "Crawling could not start: 'start_urls' not found "
+                "or empty (but found 'start_url' attribute instead, "
+                "did you miss an 's'?)"
+            )
+        for url in self.start_urls:
+            yield scrapy.Request(url=url, headers=self.headers, cookies=self.cookies, dont_filter=False)
 
+    async def parse(self, response, **kwargs):
+        # res = requests.get(url=self.start_urls[0], headers=self.headers)
+        # request = scrapy.Request(url=self.start_urls[0])
+        # print(response.text)
+        news_list = response.css("div.items-list-inner div.grid-list__item")
         for news in news_list:
             try:
                 full_text_link: str = self.base_url + news.css("div.items-list-inner__item-title a::attr(href)").get()
                 news_info: dict = await self.get_news_info(link=full_text_link)
-
+                # print(news_info)
                 yield {"author": None,
                        "title": news.css("div.items-list-inner__item-title a::text").get(),  # название
                        "brief_text": news.css("div.items-list-inner__item-preview::text").get().strip(),
@@ -74,4 +119,3 @@ class AcademInfoSpider(scrapy.Spider):
                     "images_urls": " ".join(images_urls),
                     "published_at": script_content_js.get("datePublished").strip()
                     }
-
